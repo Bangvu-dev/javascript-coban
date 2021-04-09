ce312ad1670f90d9981dffe60aa4853ee73baad2
Bangvu-dev <76621338+Bangvu-dev@users.noreply.github.com>
2021-04-09 21:21:43 +0700

    Update lab-6.js

--- lab/lab-6.js
+++ lab/lab-6.js
@@ -2,7 +2,7 @@
 // 2. Viết hàm lắng nghe sự kiện onClick thêm sản phảm
 // 3. Truy cập node input đẻ lấy được dữ liệu: tên; gia; hãng
 // 4. Tạo đối tượng spMoi bao gồm 3 thông tin trên
-// 5. Lấy mảng đối tượng dưới local lên. Nếu chưa tồn tại dứoi local, thì khỏi tạo môtj mảng mới
+// 5. Lấy mảng đối tượng dưới local lên. Nếu chưa tồn tại dưới local, thì khỏi tạo một mảng mới
 // 6. Thêm spMoi vào mảng đã lấy ở bước (5)
 // 7. LƯu lại mảng sau khi đã thêm sp xuống local 
 // ===========
